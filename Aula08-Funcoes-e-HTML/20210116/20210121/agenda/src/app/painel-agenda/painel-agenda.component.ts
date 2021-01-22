e276ff768a4141891f0f6889069205ebdd86ecb9
ADAOMASSO <adaomasso@gmail.com>
2021-01-21 22:39:15 -0300

    20210121-2239

---
+++ Aula08-Funcoes-e-HTML/20210116/20210121/agenda/src/app/painel-agenda/painel-agenda.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-agenda',
+  templateUrl: './painel-agenda.component.html',
+  styleUrls: ['./painel-agenda.component.css']
+})
+export class PainelAgendaComponent implements OnInit {
+
+  nome = 'Adão - app-painel-agenda';
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
